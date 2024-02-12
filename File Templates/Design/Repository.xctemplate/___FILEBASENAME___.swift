a02ed102e4057eaded29fffaa7603b509b81f5af
hades <43590014+Hades70@users.noreply.github.com>
2024-02-12 01:26:38 -0800

    repository file template

---
+++ File Templates/Design/Repository.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILEHEADER___
+//
+
+import Foundation
+
+protocol ___VARIABLE_productName:identifier___Repository {
+    
+}
+
+class Default___VARIABLE_productName:identifier___Repository: ___VARIABLE_productName:identifier___Repository {
+    private(set) var datasource: ___VARIABLE_productName:identifier___Datasource
+    
+    init(datasource: ___VARIABLE_productName:identifier___Datasource) {
+        self.datasource = datasource
+    }
+}
+
---
+++ File Templates/Design/Repository.xctemplate/___FILEBASENAME___Datasource.swift
@@ -0,0 +1,11 @@
+//
+//  ___FILEHEADER___
+//
+
+import Foundation
+
+protocol ___VARIABLE_productName:identifier___Datasource {}
+
+class Default___VARIABLE_productName:identifier___Datasource: ___VARIABLE_productName:identifier___Datasource {
+    
+}
